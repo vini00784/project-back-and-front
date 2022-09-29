06ae8d5876286a7b254ab1e646d2e58e680b0475
Vinicius Santos <vo70346@gmail.com>
2022-09-29 10:13:06 -0300

    página do aluno finalizada

--- assets/js/student.js
+++ assets/js/student.js
@@ -1,7 +1,6 @@
 'use strict'
 
 import { getStudentInfo, getSubjects } from "./studentFetch.js"
-// import createSubjectInitials from './utils/createInitials.js';
 import { createDiv, createImg, createSpan } from "./utils/createElements.js"
 
 const registration = localStorage.getItem('student')
@@ -25,13 +24,54 @@ const createProfile = (json) => {
 
 createProfile(studentInfo)
 
+const createSubjectInitials = (name) => {
+    let subjectName = name;
+    let subjectInitials = [];
+
+    let splitedName = subjectName.split(' ');
+    splitedName.forEach(index => {
+        // console.log(splitedName);
+        subjectInitials.push(index[0].toUpperCase());
+        // console.log(index[0].toUpperCase());
+        // splitedName = index[0].toUpperCase();
+    });
+
+    return subjectInitials.join('');
+}
+
 const showScores = (json) => {
     const { subjects } = json
     const scoresContainer = document.querySelector('.scores')
     
     subjects.forEach(element => {
-        console.log(element.media);
+        console.log(element.grade);
         const gradesContainer = createDiv('subject-score')
+        const studentGrade = createSpan('student-score', element.grade)
+        const progressBar = document.createElement('progress')
+        progressBar.classList.add('score-progress')
+        progressBar.max = '100'
+        progressBar.value = element.grade
+
+        if(element.grade >=70) {
+            progressBar.classList.add('approved')
+            studentGrade.classList.add('approved-score')
+        } else if(element.grade < 70 && element.grade >= 50) {
+            progressBar.classList.add('exam')
+            studentGrade.classList.add('exam-score')
+        } else {
+            progressBar.classList.add('disapproved')
+            studentGrade.classList.add('disapproved-score')
+        }
+
+        const subjectInitials = document.createElement('span')
+        subjectInitials.classList.add('subject-initials')
+        subjectInitials.textContent = createSubjectInitials(element.name)
+
+        gradesContainer.appendChild(studentGrade)
+        gradesContainer.appendChild(progressBar)
+        gradesContainer.appendChild(subjectInitials)
+
+        scoresContainer.appendChild(gradesContainer)
     })
 
 }
