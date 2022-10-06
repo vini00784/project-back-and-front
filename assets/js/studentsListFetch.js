789b5753f5aafc1f022a7e99bcd131efde4a52e9
vini00784 <vo70346@gmail.com>
2022-10-05 22:22:44 -0300

    mudança no fetch

--- assets/js/studentsListFetch.js
+++ assets/js/studentsListFetch.js
@@ -8,8 +8,8 @@ const getStudentsList = async (course) => {
     return data
 }
 
-const filterStudentsList = async (course, status) => {
-    const url = `https://api-lions-school.netlify.app/.netlify/functions/api/alunos/curso/?curso=${course}&&status=${status}`
+const filterStudentsList = async (course, status, conclusionYear) => {
+    const url = `https://api-lions-school.netlify.app/.netlify/functions/api/alunos/curso/?curso=${course}&&status=${status}&&conclusion=${conclusionYear}`
     const response = await fetch(url)
     const data = await response.json()
 
